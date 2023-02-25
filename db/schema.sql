c119ffa26ae1294fa0f56f7acaddef027697b8d0
tbgolfer86 <tbj86@outlook.com>
2023-02-25 18:30:38 -0500

    create tables in schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,30 @@
-DROP DATABASE IF EXISTS inventory_db;
--- Creates the "inventory_db" database --
-CREATE DATABASE inventory_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
--- Makes it so all of the following code will affect inventory_db --
-USE inventory_db;
+USE employee_db;
 
--- Creates the table "produce" within inventory_db --
-CREATE TABLE produce (
-  -- Creates a numeric column called "id"
-  id INT NOT NULL,
-  -- Makes a string column called "name"
-  name VARCHAR(100) NOT NULL
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
 );
