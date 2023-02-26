b1a0b818ade68b4370515e0bc7c7a415fff32314
tbgolfer86 <tbj86@outlook.com>
2023-02-25 21:49:06 -0500

    added to seed file

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (name)
+VALUES ("Accounting"),
+       ("Human Resources"),
+       ("IT"),
+       ("Marketing");
+
+INSERT INTO roles (title, salary)
+VALUES ("Manager", 100000),
+       ("Associate", 55000),
+       ("Manager", 110000),
+       ("Intern", 32000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("John", "Smith"),
+       ("Mike", "Jones"),
+       ("John", "Lennon"),
+       ("Happy", "Gilmore");
+
