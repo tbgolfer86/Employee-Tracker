448695cce8182e9d7c3cb2d85aff4c61a5b7f776
tbgolfer86 <tbj86@outlook.com>
2023-02-26 20:12:11 -0500

    added connection to database

---
+++ index.js
@@ -0,0 +1,16 @@
+const consoleTable = require('console.table');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'password',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
