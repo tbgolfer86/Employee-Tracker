e06d63218dc7cda8409cb70d97833fc1b7386dc4
tbgolfer86 <tbj86@outlook.com>
2023-02-26 20:43:09 -0500

    added values to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,17 +2,19 @@ INSERT INTO department (name)
 VALUES ("Accounting"),
        ("Human Resources"),
        ("IT"),
+       ("Technology"),
        ("Marketing");
 
-INSERT INTO roles (title, salary)
-VALUES ("Manager", 100000),
-       ("Associate", 55000),
-       ("Manager", 110000),
-       ("Intern", 32000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 100000, 1),
+       ("Associate", 55000, 5),
+       ("Engineer", 110000, 4),
+       ("Intern", 32000, 2),
+       ("Executive", 222000, 3);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ("John", "Smith"),
-       ("Mike", "Jones"),
-       ("John", "Lennon"),
-       ("Happy", "Gilmore");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 1, 1),
+       ("Mike", "Jones", 1, 2),
+       ("John", "Lennon", 1, 3),
+       ("Happy", "Gilmore", 1, 4);
 
